a8cad6a7c5cec263bdbc027e14e4af70789035b7
Ayush Hakhu <57040928+ayushhakhu@users.noreply.github.com>
2022-01-26 13:43:05 +0530

    updating for reporting

---
+++ sendReport.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+(async function sendMailFunction() {
+    await console.log("Sending Report Over Mail");
+
+    const transporter = await nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user:process.env.MAIL_USER_EMAIL,
+            pass:process.env.MAIL_USER_PASSWORD
+        },
+    })
+
+    await transporter.sendMail({
+        from:process.env.MAIL_FROM,
+        to:process.env.MAIL_TO,
+        subject: "Daily Report",
+        text: "Please find daily report",
+        html: "<b>Hello world?</b>",
+    });
+})()
