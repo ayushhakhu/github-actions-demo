2a2c7ee40db127d5875ca9e5f627370db2b1808f
ayushhakhu <57040928+ayushhakhu@users.noreply.github.com>
2022-02-18 19:09:55 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -137,7 +137,22 @@ exports.config = {
     // see also: https://webdriver.io/docs/dot-reporter
     reporters: ['spec', ['json', {
         outputDir: './Results'
-    }]],
+    }],
+          ["html-nice", {
+            outputDir: './Results/html-reports/',
+            filename: 'report.html',
+            reportTitle: 'Test Report Title',
+            linkScreenshots: true,
+            //to show the report in a browser when done
+            showInBrowser: true,
+            collapseTests: false,
+            //to turn on screenshots after every test
+            useOnAfterCommandForScreenshot: false,
+
+            //to initialize the logger
+//             LOG: log4j.getLogger("default")
+        }
+        ]],
 
 
     //
